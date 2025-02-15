1db7d52c7de2898bb814d682ada6e083043b533b
Martin Clausen <martin.clausen@gmail.com>
2025-02-15 18:01:16 +0100

    Fix nesting of connected indicator.

--- src-tauri/src/templates.rs
+++ src-tauri/src/templates.rs
@@ -62,8 +62,9 @@ pub fn connected() -> Markup {
         div {
             span."inline-flex items-center bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300" {
                 span."w-2 h-2 me-1 bg-green-500 rounded-full" {}
+                "Connected"
             }
-            "Connected"
+
         }
     }
 }
